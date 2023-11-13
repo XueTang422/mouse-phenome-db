17d3f16dcb7b613f4e7dbc203c837838e5e0bc62
XueTang422 <tang.1485@osu.edu>
2023-11-13 17:19:08 -0500

    Create skeletons for two homepage prototypes; connect homepages to react app

--- mpd-app/src/App.js
+++ mpd-app/src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage1 from './HomePage1';
+import HomePage2 from './HomePage2';
+
+
+const PROTOTYPE_VERSION = 2;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { PROTOTYPE_VERSION === 1 ? (<HomePage1/>) : (<HomePage2/>)}
       </header>
     </div>
   );
