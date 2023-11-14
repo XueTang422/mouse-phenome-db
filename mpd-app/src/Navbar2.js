a6297b4b5b89d87660eb773a0680626a7df18c04
XueTang422 <tang.1485@osu.edu>
2023-11-14 16:54:32 -0500

    Implement styles for navbar logo, nav items, and search box

--- mpd-app/src/Navbar2.js
+++ mpd-app/src/Navbar2.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import './Navbar2.css';
 import lightThemeLogo from './images/logo2.png';
+import magnifyingGlass from './images/magnifying-glass.svg'
 
 const Navbar2 = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <a href="https://phenome.jax.org/">
-            <img src={lightThemeLogo} alt="NavbarLogo2" />
+          <img className="logo" src={lightThemeLogo} alt="NavbarLogo2"/>
         </a>
       </div>
       <ul className="navbar-menu">
         <li className="navbar-item"><a href="#">Get Started</a></li>
         {/* TO-DO: Modify href placeholder navigating to a specific url */}
-        <li className="navbar-item"><a href="#">Explore</a></li>
-        <li className="navbar-item"><a href="#">Analyze</a></li>
+        <li className="navbar-item">
+          <a href="#">
+            Explore<i class="arrow down"></i>
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a href="#">
+            Analyze<i class="arrow down"></i>
+          </a>
+        </li>
         <li className="navbar-item"><a href="#">Contribute</a></li>
         <li className="navbar-item"><a href="#">Download</a></li>
         <li className="navbar-item"><a href="#">About</a></li>
       </ul>
       <div className="navbar-search-signin">
-        <input type="text" className="search-input" placeholder="Search" />
+        <div className="search-box">
+          <img className="search-glass" src={magnifyingGlass} alt="magnifying glass"/>
+          <input className="search-text" type="text" placeholder="Search" />
+        </div>
         <button className="sign-in-button">Sign In</button>
       </div>
     </nav>
