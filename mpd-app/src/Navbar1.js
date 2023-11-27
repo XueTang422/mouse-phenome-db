029c4399a127e4a55b4d86f965a05bc073a5107d
zzeng01 <clessej119@gmail.com>
2023-11-27 01:28:14 -0500

    add sidebar

--- mpd-app/src/Navbar1.js
+++ mpd-app/src/Navbar1.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Navbar1.css';
 import lightThemeLogo from './images/logo1.png';
 import magnifyingGlass from './images/magnifying-glass.svg' // import search glass
 import { FaBars } from "react-icons/fa6";
-import Sidebar from './Sidebar.js'
+import Sidebar from "./Sidebar"
+import { SidebarData } from "./SidebarData";
 const Navbar1 = () => {
+
+  const [sidebar, setSidebar]=useState(false)
+
+  const showSidebar =()=> setSidebar(!sidebar)
+
+
   return (
-    <nav className="navbar">
+    <>
+    <nav className="navbar-dark">
       <div className='navbar-fabars-logo'>
         <div className="navbar-fabars">
-          <FaBars className='fabars'  onClick={Sidebar}/>
+          <FaBars className='fabars'  onClick={showSidebar}/>
         </div>
 
         <div className="navbar-logo">
@@ -31,6 +39,15 @@ const Navbar1 = () => {
         <button className="signin-button">Sign In</button>
       </div>
     </nav>
+
+    <nav className={sidebar? "sidebar active": "sidebar"} >
+      <ul>
+      { SidebarData.map((item, index) => <Sidebar key={index} item={item} status={sidebar}/>) }
+      </ul>
+
+    </nav>
+
+    </>
   );
 };
 
